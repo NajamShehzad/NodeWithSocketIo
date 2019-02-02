b62bc07bc5c6b904720e6619e462fbde7b810739
NajamShehzad <najambutt195@gmail.com>
2019-02-02 18:46:32 +0500

    Database Configuration Completed

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+require('./config')(app);
+require('./db/repository')(app,mongoose);
 require('./routes')(app, mongoose);
-
-
+require('./model')(app, mongoose);
 
 
 // catch 404 and forward to error handler
