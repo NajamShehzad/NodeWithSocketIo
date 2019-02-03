09d6a1733db96bc4dc4b3625515953cf2859fbe6
NajamShehzad <najambutt195@gmail.com>
2019-02-03 13:05:05 +0500

    Prettier JS

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
-
-exports = module.exports = function (app, mongoose) {
-
-  var express = require('express');
+exports = module.exports = function(app, mongoose) {
+  var express = require("express");
   var router = express.Router();
 
   /* GET users listing. */
-  router.get('/', function (req, res, next) {
-    let User = new app.db.models.User({ name: "Najam", email: "najam@gmail.com" });
+  router.get("/", function(req, res, next) {
+    let User = new app.db.models.User({
+      name: "Najam",
+      email: "najam@gmail.com"
+    });
     User.save().then(data => {
       res.send(data);
     });
   });
 
-  app.use('/user', router);
+  app.use("/user", router);
 };
